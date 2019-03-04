2c4882d2f5fe21172378fa62b093e9265ca6b297
umcamaleao <48225010+umcamaleao@users.noreply.github.com>
2019-03-04 18:24:08 -0300

    Update cortar_gastos.py

--- cortar_gastos.py
+++ cortar_gastos.py
@@ -1,19 +1,19 @@
 from itertools import accumulate
 
 def patrimonio_anual(anos, inicial, corte_por_ano, juros):
-    renda_anual = [inicial] + [corte_por_ano for _ in range(anos)]
-    return list(accumulate(renda_anual, lambda total, anual: total*juros + anual))
+	renda_anual = [inicial] + [corte_por_ano for _ in range(anos)]
+	return list(accumulate(renda_anual, lambda total, anual: total*juros + anual))
 
 def cozinhar(anos, inicial=1000, corte_por_ano=1820):
-    return patrimonio_anual(anos=anos, inicial=inicial, corte_por_ano=corte_por_ano, juros=1.04)
+	return patrimonio_anual(anos=anos, inicial=inicial, corte_por_ano=corte_por_ano, juros=1.04)
 
 def investir(anos, inicial=1000, juros=1.06):
-    return patrimonio_anual(anos=anos, inicial=inicial, corte_por_ano=0, juros=juros)
+	return patrimonio_anual(anos=anos, inicial=inicial, corte_por_ano=0, juros=juros)
 
 def comparar(inicial, juros=1.06, corte_por_ano=1820):
-    return(inicial,
-           cozinhar(anos=10, inicial=inicial, corte_por_ano=corte_por_ano)[-1],
-           investir(anod=10, inicial=inicial, juros=juros)[-1])
+	return(inicial,
+		   cozinhar(anos=10, inicial=inicial, corte_por_ano=corte_por_ano)[-1],
+		   investir(anod=10, inicial=inicial, juros=juros)[-1])
 
 
 if __name__ == '__main__':
